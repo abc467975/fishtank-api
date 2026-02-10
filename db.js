752ea4863985735659ad72feb29b1480693fca4b
abc467975 <abc467975@gmail.com>
2026-02-10 13:56:49 +0800

    refactor mongodb connection to use env

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const MONGO_URI =
-  "mongodb+srv://abc467975:aA46797521@cluster0.7oj9nmn.mongodb.net/fish";
-
+  "process.env.MONGO_URI"
 mongoose.connect(MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB error", err));
