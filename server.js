048e62236562a307aae86a54ea868de96120f689
abc467975 <abc467975@gmail.com>
2026-02-10 14:24:50 +0800

    refactor mongodb connection to use env

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 require("./db");
-
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
 app.use(express.json());
 app.use("/api", require("./routes/ingest"));
 app.use("/api", require("./routes/query"));
